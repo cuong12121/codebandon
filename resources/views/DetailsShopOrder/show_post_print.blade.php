d4ee3cea5eccea3e07456385e4e7b14452c04e5c
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-14 13:28:25 +0700

    Update show_post_print.blade.php

--- resources/views/DetailsShopOrder/show_post_print.blade.php
+++ resources/views/DetailsShopOrder/show_post_print.blade.php
@@ -60,7 +60,54 @@
         </thead>
         <tbody>
             <?php 
-                $dem = 0;
+                $gio_array = [
+                    0 => "-- Giờ --",
+                    1 => "7H",
+                    2 => "18H15",
+                    3 => "8H",
+                    4 => "8H30",
+                    5 => "10H",
+                    6 => "11H",
+                    7 => "13H",
+                    8 => "15H30",
+                    9 => "9H",
+                    10 => "14H",
+                    11 => "15H",
+                    12 => "16H",
+                    13 => "7H10", // Value 13 tương ứng với 7H10
+                    14 => "9H30", // Value 14 tương ứng với 9H30
+                    15 => "14H30",
+                    16 => "12H30",
+                    17 => "22H",
+                    18 => "12H40",
+                    19 => "17H",
+                    20 => "16H20",
+                    21 => "18H"
+                ];
+                $kho_array = [
+                    0 => "-- Kho --",
+                    1 => "Kho Hà Nội",
+                    2 => "Kho Hồ Chí Minh",
+                    4 => "Kho hàng Cao Duy Hoan",
+                    6 => "Kho Văn La",
+                    7 => "Kho Văn Phú"
+                ];
+
+                $san_array = [
+                    0 => "-- Sàn --",
+                    1 => "Lazada",
+                    2 => "Shopee",
+                    3 => "Tiki",
+                    4 => "Lex ngoài HCM",
+                    6 => "Đơn ngoài",
+                    8 => "Best",
+                    9 => "Ticktok",
+                    10 => "Viettel",
+                    11 => "Shopee ngoài",
+                    12 => "Giao hàng tiết kiệm",
+                    13 => "Giao hàng nhanh"
+                ];
+                                $dem = 0;
             ?>
             @foreach($data as $value)
             <?php 
@@ -68,9 +115,9 @@
             ?>
             <tr>
                 <td>{{ $dem }}</td>
-                <td>{{ $value['house_id'] }}</td>
-                <td>{{ $value['platform_id'] }}</td>
-                <td>{{ $value['warehouse_id'] }}</td>
+                <td>{{ $gio_array[$value['house_id']] }}</td>
+                <td>{{ $san_array[$value['platform_id']] }}</td>
+                <td>$kho_array[{{ $value['warehouse_id']] }}</td>
                 <td>Đủ hàng</td>
                 <td>
                     <a href="{{ route('show-print', $value['id']) }}">Duyệt hàng</a> 
