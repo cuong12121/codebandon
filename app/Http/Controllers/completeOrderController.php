fec96eb2b0a7bed08072e26c7f2614a7756eb955
cuong12121 <cuongdao12121@gmail.com>
2025-07-15 11:17:06 +0700

    Update completeOrderController.php

--- app/Http/Controllers/completeOrderController.php
+++ app/Http/Controllers/completeOrderController.php
@@ -111,33 +111,20 @@ class completeOrderController extends Controller
 
         if ($id) {
 
-            // Key Redis bạn muốn dùng
-            $redisKey = 'excel_data_stock';
+           
+            // Load file
+            $spreadsheet = IOFactory::load($filePath);
 
-            // Thử lấy từ Redis trước
-            $data_redis = $redis->get($redisKey);
+            // Lấy sheet đầu tiên
+            $sheet = $spreadsheet->getActiveSheet();
 
-            if (!$data_redis) {
-                // Load file
-                $spreadsheet = IOFactory::load($filePath);
+            // Lấy toàn bộ dữ liệu thành mảng
+            $datas = $sheet->toArray();
 
-                // Lấy sheet đầu tiên
-                $sheet = $spreadsheet->getActiveSheet();
-
-                // Lấy toàn bộ dữ liệu thành mảng
-                $datas = $sheet->toArray();
-
-                // Lưu vào Redis, ví dụ: trong 10 phút (600 giây)
-                $redis->setex($redisKey, 600, $datas);
-            } else {
-                // Nếu có dữ liệu Redis rồi thì decode lại thành mảng
-                $datas = json_decode($data_redis, true);
-            }
-
-
-            $inventory = [];
-
-            dd($data_redis);
+            dd($datas);
+            // Lưu vào Redis, ví dụ: trong 10 phút (600 giây)
+            $redis->setex($redisKey, 600, $datas);
+          
 
             for ($i = 2; $i < count($datas); $i++) {
                 $itemCode = $datas[$i][0];
