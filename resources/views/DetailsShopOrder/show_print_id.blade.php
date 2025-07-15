c623493d4478cffadaf754aba496aac956fd1750
cuong12121 <cuongdao12121@gmail.com>
2025-07-15 10:09:58 +0700

    Update show_print_id.blade.php

--- resources/views/DetailsShopOrder/show_print_id.blade.php
+++ resources/views/DetailsShopOrder/show_print_id.blade.php
@@ -304,11 +304,7 @@ document.getElementById('confirm').addEventListener('submit', function(e) {
 <script>
 
 
-    const stockData = @json(collect(json_decode(Redis::get('stock_data'), true))
-            ->map(fn($qty, $sku) => ['sku' => $sku, 'qty' => $qty])
-            ->values()
-            ->all()
-    );
+    
   function openPopup() {
     document.getElementById('popupOverlay').style.display = 'block';
     const tbody = document.getElementById('stockTableBody');
